ae94b08e11e8ab20cef78cb797cd993de1ca804e
abstract-animation-9914 <vm930@hotmail.com>
2018-11-09 14:56:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ function add2(n) {
   // Feel free to move things around!
   
 }
+var theFunk = funckyFunction
 
 var funkyFunction = function() {
   return function() {
@@ -29,4 +30,4 @@ var funkyFunction = function() {
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
 
-var theFunk = funckyFunction ;
+
